b8d99d93b9c84a7c31cb9b43bfca8f2b7c231d2a
moiseevanton <athidif94@gmail.com>
2016-04-27 00:28:25 -0400

    Initial Commit

---
+++ Largest_number_in_array/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Largest_number_in_array
+//
+//  Created by Anton Moiseev on 2016-04-27.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
